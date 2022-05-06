ff114218a9a3e1fcbf905b0c3cb4d99998881d4d
Viviane <oviviane772@gmail.com>
2022-05-06 20:59:44 -0300

    Proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
-
-export function Dashboard(){
-  return(
+export function Dashboard() {
+  return (
     <Container>
-      <Title>
-        Dashboard
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/34073446?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá ,</UserGreeting>
+              <UserName>Viviane</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
-  )
+  );
 }
-
