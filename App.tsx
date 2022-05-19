f52800831dff6c4c2fac6c3e05613a2731fbdbe8
Viviane <oviviane772@gmail.com>
2022-05-19 07:50:44 -0300

    Navegação

--- App.tsx
+++ App.tsx
@@ -13,8 +13,9 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import {Register} from './src/screens/Register';
-import {CategorySelect} from './src/screens/CategorySelect';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {AppRoutes} from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +29,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
